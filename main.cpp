5104dbc4ce65178a4a79e3aaf52bf60a5c57235f
bardia16 <31382042+bardia16@users.noreply.github.com>
2023-06-15 00:23:17 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+
+class TCPConnection {
+private:
+    int cwnd;
+    int ssthresh;
+    int rtt;
+    bool retransmitting;
+    bool fast_recovery;
+    int sending_rate;
+
+public:
+    TCPConnection(int initial_cwnd = 1, int initial_ssthresh = 65535) {
+        cwnd = initial_cwnd;
+        ssthresh = initial_ssthresh;
+        rtt = 0;
+        retransmitting = false;
+        fast_recovery = false;
+        sending_rate = 1;
+    }
+
+    void onPacketLoss() {
+        ssthresh = std::max(2, cwnd / 2);
+        cwnd = 1;
+        fast_recovery = true;
+    }
+
+    void SendData() {
+        if (fast_recovery) {
+            cwnd += 1;
+        }
+        else {
+            cwnd += std::min(1, sending_rate);
+        }
+        cwnd = std::min(cwnd, ssthresh);
+        sending_rate = cwnd;
+    }
+
+    void onRTTUpdate(int new_rtt) {
+        rtt = new_rtt;
+        if (!fast_recovery) {
+            if (cwnd < ssthresh) {
+                cwnd += 1;
+            }
+            else {
+                cwnd += 1 / cwnd;
+            }
+        }
+        else {
+            cwnd = ssthresh;
+            fast_recovery = false;
+        }
+    }
+
+    int getCwnd() const {
+        return cwnd;
+    }
+
+    int getSsthresh() const {
+        return ssthresh;
+    }
+    int getRTT() {
+        return rtt;
+    }
+};
+
+int main() {
+    int index = 0;
+    // Create an array of TCPConnection objects with different initial congestion window and slow start threshold values
+    TCPConnection connections[] = {
+        TCPConnection(10, 5),
+        TCPConnection(5, 2),
+        TCPConnection(5, 10),
+        TCPConnection(2, 5)
+    };
+
+    // Simulate sending data using current congestion window and slow start threshold for each connection
+    for (auto& conn : connections) {
+        index += 1;
+        std::cout<< "After rtt update"<<std::endl;
+        std::cout << "CWND: " << conn.getCwnd() << ", ssthresh: " << conn.getSsthresh() << ", RTT: " << conn.getRTT() << std::endl;
+        std::cout << "Sending data using current congestion window and slow start threshold for connection " << index << "...\n";
+        conn.SendData();
+        std::cout << "CWND: " << conn.getCwnd() << ", ssthresh: " << conn.getSsthresh() << ", RTT: " << conn.getRTT() << std::endl;
+
+
+        // Simulate packet loss for each connection
+        std::cout << "Packet loss detected for connection " << index << "\n";
+        conn.onPacketLoss();
+
+        // Simulate sending data again, but with reduced congestion window and slow start threshold for each connection
+        std::cout << "Sending data again using reduced congestion window and slow start threshold for connection " << index << "...\n";
+        conn.SendData();
+        std::cout << "CWND: " << conn.getCwnd() << ", ssthresh: " << conn.getSsthresh() << ", RTT: " << conn.getRTT() << std::endl;
+    }
+
+    return 0;
+}
